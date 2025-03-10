cefe98473ccc9da9e92bc82d1ae6f1f03c0ce929
amlimjoc <amlimjoc@uci.edu>
2025-03-10 21:54:30 +0000

    Created error

---
+++ src/ladder.cpp
@@ -0,0 +1,17 @@
+#include "ladder.h"
+
+void error(string word1, string word2, string msg) {
+    cout << "Error: " << msg << << " [" << word1 << ", " << word2 << "]" << endl;
+}
+
+bool edit_distance_within(const string& str1, const string& str2, int d) {
+
+}
+
+bool is_adjacent(const string& word1, const string& word2) {
+
+}
+
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+    
+}
