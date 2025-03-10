98ca169e23551bd5379c2194a0a57a7b59f90480
amlimjoc <amlimjoc@uci.edu>
2025-03-10 22:30:54 +0000

    Created is_adjacent, rough

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -10,8 +10,37 @@ bool edit_distance_within(const string& str1, const string& str2, int d) {
 }
 
 bool is_adjacent(const string& word1, const string& word2) {
+    int len1 = word1.length(), len2 = word2.length();
+    
+    if (abs(len1 - len2) > 1) return false;
 
+    if (len1 == len2) { 
+        // same length
+        int diff_count = 0;
+        for (int i = 0; i < len1; i++) {
+            if (word1[i] != word2[i]) diff_count++;
+            if (diff_count > 1) return false;
+        }
+        return diff_count == 1;
+    } 
+
+    if (len1 > len2) swap(word1, word2);
+    
+    // different elngths
+    int i = 0, j = 0, diff_count = 0;
+    while (i < len1 && j < len2) {
+        if (word1[i] != word2[j]) {
+            diff_count++;
+            if (diff_count > 1) return false;
+            j++;
+        } else {
+            i++;
+            j++;
+        }
+    }
+    return true;
 }
+
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
     queue<vector<string>> ladder_queue;
