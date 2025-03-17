dcc964b049afb65bc0bcb66bd3c07280a6ca0e99
amlimjoc <amlimjoc@uci.edu>
2025-03-17 06:43:00 +0000

    fixed ladder

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,130 +1,111 @@
 #include "ladder.h"
-#include <unordered_set>
 
+using namespace std;
+
+// Prints an error message
 void error(string word1, string word2, string msg) {
     cout << "Error: " << msg << " [" << word1 << ", " << word2 << "]" << endl;
 }
 
+// Check if the edit distance between two words is within `d`
 bool edit_distance_within(const string& str1, const string& str2, int d) {
-    int len1 = str1.length(), len2 = str2.length();
-
-    if (abs(len1 - len2) > d) return false;
-    
-    vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1));
-
-    for (int i = 0; i <= len1; i++) dp[i][0] = i;
-    for (int j = 0; j <= len2; j++) dp[0][j] = j;
-
-    for (int i = 1; i <= len1; i++) {
-        for (int j = 1; j <= len2; j++) {
-            if (str1[i - 1] == str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1];
-            } else {
-                dp[i][j] = min(min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]) + 1;
-            }
-        }
-    }
-
-    return dp[len1][len2] <= d;
-}
-
-bool is_adjacent(const string& word1, const string& word2) {
-    int len1 = word1.length(), len2 = word2.length();
-    
+    int len1 = str1.size(), len2 = str2.size();
+    if (str1 == str2) return true;
     if (abs(len1 - len2) > 1) return false;
 
-    string shorter = word1, longer = word2;
-    if (len1 > len2) swap(shorter, longer);
+    int i = 0, j = 0, difference = 0;
+    while (i < len1 && j < len2) {
+        if (str1[i] != str2[j]) {
+            difference++;
+            if (difference > d) return false;
+            if (len1 > len2) i++; 
+            else if (len1 < len2) j++; 
+            else { i++; j++; }
+        } else { i++; j++; }
+    }
     
-    if (shorter.length() == longer.length()) { 
-        int diff_count = 0;
-        for (int i = 0; i < shorter.length(); i++) {
-            if (shorter[i] != longer[i]) diff_count++;
-            if (diff_count > 1) return false;
-        }
-        return diff_count == 1;
-    }
-
-    int i = 0, j = 0, diff_count = 0;
-    while (i < shorter.length() && j < longer.length()) {
-        if (shorter[i] != longer[j]) {
-            diff_count++;
-            if (diff_count > 1) return false;
-            j++;
-        } else {
-            i++;
-            j++;
-        }
-    }
-    return true;
+    if (i < len1 || j < len2) difference++;
+    return difference == d;
 }
 
+// Check if two words are adjacent (edit distance of 1)
+bool is_adjacent(const string& word1, const string& word2) {
+    return edit_distance_within(word1, word2, 1);
+}
 
+// Generates the shortest word ladder using BFS
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
-    queue<vector<string>> ladder_queue;
-    ladder_queue.push({begin_word});
-    unordered_set<string> visited_words;
-    visited_words.insert(begin_word);
+    if (begin_word == end_word) return {};
 
-    while(!ladder_queue.empty()) {
-        vector<string> ladder = ladder_queue.front();
-        ladder_queue.pop();
-        string last_word = ladder.back();
+    set<string> local_set = word_list;
+    queue<vector<string>> queue_path;
+    queue_path.push({begin_word});
+    local_set.erase(begin_word);
 
-        for (const string&word : word_list) {
-            if (is_adjacent(last_word, word) && visited_words.find(word) == visited_words.end() ) {
-                vector<string> new_ladder = ladder;
-                new_ladder.push_back(word);
-                visited_words.insert(word);
+    while (!queue_path.empty()) {
+        vector<string> word_ladder = queue_path.front();
+        queue_path.pop();
+        string cur_word = word_ladder.back();
 
-                if (word == end_word) return new_ladder; // word found
+        if (cur_word == end_word) return word_ladder;
 
-                ladder_queue.push(new_ladder);
+        for (auto word = local_set.begin(); word != local_set.end();) {
+            if (is_adjacent(cur_word, *word)) {
+                vector<string> updated_ladder = word_ladder;
+                updated_ladder.push_back(*word);
+                queue_path.push(updated_ladder);
+                word = local_set.erase(word);
+            } else {
+                ++word;
             }
         }
     }
-    return {}; // not found
+    return {};
 }
 
-void load_words(set<string> & word_list, const string& file_name) {
-    ifstream file(file_name);
-    if (!file) { 
-        error("","", "Could not open file.");
-        return;    
+// Loads words from a file into a set
+void load_words(set<string>& word_list, const string& file_name) {
+    ifstream curFile(file_name);
+    if (!curFile.is_open()) {
+        error("", "", "Cannot open file!");
+        return;
     }
-
-    string word;
-    while (file >> word) {
-        word_list.insert(word);
+    for (string word; getline(curFile, word);) {
+        if (!word.empty()) word_list.insert(word);
     }
-    file.close();
+    curFile.close();
 }
 
+// Prints the word ladder
 void print_word_ladder(const vector<string>& ladder) {
-    if (ladder.empty()) { error("","", "Ladder is empty or not found.");}
-
-    for (size_t i = 0; i < ladder.size(); ++i) {
-        cout << ladder[i];
-        if (i < ladder.size() - 1) {cout << " -> ";}
+    if (!ladder.empty()) {
+        cout << "Word ladder found: ";
+        for (const auto& word : ladder) {
+            cout << word << " ";
+        }
+        cout << endl;
+    } else {
+        error("", "", "No word ladder found.");
     }
-    cout << endl;
 }
 
-void verify_word_ladder(){
+// Verifies if the generated word ladder is valid
+void verify_word_ladder() {
     set<string> word_list;
     load_words(word_list, "src/words.txt");
 
     vector<string> ladder = generate_word_ladder("code", "data", word_list);
-    if (ladder.empty()) { error("code", "data", "No ladder found."); }
+    if (ladder.empty()) {
+        error("code", "data", "No ladder found.");
+        return;
+    }
 
     for (size_t i = 0; i < ladder.size() - 1; i++) {
         if (!is_adjacent(ladder[i], ladder[i + 1])) {
             error(ladder[i], ladder[i + 1], "Words not adjacent.");
         }
-
-        
     }
+
     print_word_ladder(ladder);
     cout << "Word ladder works." << endl;
-}   
-
+}
