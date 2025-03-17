3c33990f8d4d0fa427ea0cac81768f3e137cca08
amlimjoc <amlimjoc@uci.edu>
2025-03-17 05:50:46 +0000

    Created dijkstras main

--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -1,16 +1,16 @@
 #include "dijkstras.h"
+using namespace std;
 
 int main() {
     Graph G;
-    file_to_graph("graph.txt", G); // Load graph from file
+    file_to_graph("./src/medium.txt", G);
 
-    int source = 0, destination = 4;
-    vector<int> previous;
-    
-    vector<int> distances = dijkstra_shortest_path(G, source, previous);
-    vector<int> shortestPath = extract_shortest_path(distances, previous, destination);
-
-    print_path(shortestPath, distances[destination]);
+    int src = 3
+    vector<int> prev;
+    vector<int> dijkstra_path = dijkstra_shortest_path(G, src, prev);
+    int destination = 1;
+    vector<int> short_path = extract_shortest_path(dijkstra_path, prev, destination);
+    print_path(short_path, dijkstra_path[destination]);
 
     return 0;
 }
